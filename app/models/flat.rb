86e3eed2e95cb752a8d6fdf2b47501fe31459a98
Bryan Leighton <bryan.leigh77@gmail.com>
2018-10-16 09:12:16 +0200

    add inclusion validation to boolean values in models

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,7 +2,7 @@ class Flat < ApplicationRecord
   belongs_to :location
   has_many :tenants, through: :flat_contracts
   validates :flat_type, presence: true
-  validates :availability, presence: true
+  validates :availability, inclusion: { in: [ true, false ] }
   validates :address, presence: true
   validates :occupancy, presence: true
   validates :bedrooms, presence: true
