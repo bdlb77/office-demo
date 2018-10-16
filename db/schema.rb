90e419bca936d6fe01348cf67144c0e896027cd5
Bryan Leighton <bryan.leigh77@gmail.com>
2018-10-16 22:12:52 +0200

    added seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_212142) do
+ActiveRecord::Schema.define(version: 2018_10_16_194956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_10_15_212142) do
     t.boolean "open"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
   end
 
   create_table "tenants", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#  Creation of user
+require 'faker'
+FlatContract.delete_all
+Tenant.delete_all
+Flat.delete_all
+Location.delete_all
+User.delete_all
+
+
+user = User.create!(email: 'bdlb@utexas.edu', password:'123456')
+
+location = Location.create!(name: 'La Défense', city: 'Paris', open: true)
+
+5.times do
+	boolean = [true, false].shuffle.first
+	flat_type = ['2 Bedroom', 'Studio', '3 Bedroom'].shuffle.first
+	Flat.create!(flat_type: flat_type, address: Faker::Address.full_address, availability: boolean, floor_number: rand(1..3), occupancy: rand(1..5), bedrooms: rand(1..3), bathrooms: rand(1..2), location: location )
+end
+
+puts "flats created"
+
+6.times do 
+	Tenant.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email:'steve@swbell.net', phone_number: Faker::PhoneNumber.cell_phone, username: Faker::Name.initials, birthday: '06.08.1993')
+end
+puts "tenants created"
+7.times do
+	number = rand(1..5)
+	FlatContract.create!(flat: Flat.find(number), tenant: Tenant.find(number), start_date: Faker::Date.between(4.days.ago, 3.days.ago), end_date:Faker::Date.between(2.days.ago, Date.today), user: user )
+end
+puts "flat contracts created"
