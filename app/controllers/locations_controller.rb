6cfbef46f10a913755c3561ad39c2889d1aac343
Bryan Leighton <bryan.leigh77@gmail.com>
2018-10-16 09:34:15 +0200

    add location view index and show page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,15 @@
 class LocationsController < ApplicationController
+		before_action :set_location, only: [:show]
 	def index
 		@locations = Location.all
+	end
+
+	def show
+	end
+
+	private
+
+	def set_location
+		@location = Location.find(params[:id])
 	end
 end
