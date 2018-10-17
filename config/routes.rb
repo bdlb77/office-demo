4e436c7089ba0e33d9939a11626eb6f874a2edc4
Bryan Leighton <bryan.leigh77@gmail.com>
2018-10-17 08:20:52 +0200

    basic routes between tenants, locations, flats

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :locations, only: [:index, :show]
+  resources :locations, only: [:index, :show] do
+  	resources :flats, only: [:show]
+  end
+  resources :tenants, only: [:index, :show]
 end
