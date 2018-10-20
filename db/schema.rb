7c6f75ad1ce0754bfc05ad222b4259727185dbf6
Bryan Leighton <bryan.leigh77@gmail.com>
2018-10-20 16:43:39 +0200

    responsive flat show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_093651) do
+ActiveRecord::Schema.define(version: 2018_10_19_135126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_093651) do
     t.bigint "location_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "flat_number"
     t.index ["location_id"], name: "index_flats_on_location_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -19,21 +20,27 @@ user = User.create!(email: 'bdlb@utexas.edu', password:'123456', admin: true)
 user2 = User.create!(email:'steve@hotmail.com', password: '123456', admin: false)
 
 location = Location.create!(name: 'La Défense', city: 'Paris', open: true)
+location2 = Location.create!(name: "L'Auberge", city: 'Paris', open: true)
 
-5.times do
-	boolean = [true, false].shuffle.first
-	flat_type = ['2 Bedroom', 'Studio', '3 Bedroom'].shuffle.first
-	Flat.create!(flat_type: flat_type, address: Faker::Address.full_address, availability: boolean, floor_number: rand(1..3), occupancy: rand(1..5), bedrooms: rand(1..3), bathrooms: rand(1..2), location: location )
-end
+
+	flat_type = ['2-Bedroom', 'Studio', '3-Bedroom'].shuffle.first
+
+	Flat.create!(flat_type: "2-Bedroom", address: Faker::Address.full_address, availability: true, floor_number: 3, occupancy: 2, bedrooms: 2, bathrooms: 1, location: location, flat_number: "301" )
+	Flat.create!(flat_type: "2-Bedroom", address: Faker::Address.full_address, availability: true, floor_number: 1, occupancy: 4, bedrooms: 2, bathrooms: 1, location: location, flat_number: "101")
+	Flat.create!(flat_type: "Studio", address: Faker::Address.full_address, availability: true, floor_number: 1, occupancy: 1, bedrooms: 1, bathrooms: 1, location: location, flat_number: "110" )
+	Flat.create!(flat_type: "Studio", address: Faker::Address.full_address, availability: true, floor_number: 1, occupancy: 1, bedrooms: 1, bathrooms: 2, location: location2, flat_number: "102" )
+	Flat.create!(flat_type: "3-Bedroom", address: Faker::Address.full_address, availability: true, floor_number: 2, occupancy: 3, bedrooms: 3, bathrooms: 2, location: location2, flat_number: "203" )
+	Flat.create!(flat_type: "3-Bedroom", address: Faker::Address.full_address, availability: true, floor_number: 2, occupancy: 5, bedrooms: 3, bathrooms: 2, location: location2, flat_number:"215" )
+	Flat.create!(flat_type: "3-Bedroom", address: Faker::Address.full_address, availability: true, floor_number: 2, occupancy: 4, bedrooms: 3, bathrooms: 2, location: location2, flat_number:"220" )
 
 puts "flats created"
 
-6.times do 
-	Tenant.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email:'steve@swbell.net', phone_number: Faker::PhoneNumber.cell_phone, username: Faker::Name.initials, birthday: '06.08.1993')
+10.times do 
+	Tenant.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email:'example@gmail.com', phone: Faker::PhoneNumber.cell_phone, username: Faker::Name.initials, birthday: '06.08.1993')
 end
 puts "tenants created"
-7.times do
-	number = rand(1..5)
+10.times do
+	number = rand(1..7)
 	FlatContract.create!(flat: Flat.find(number), tenant: Tenant.find(number), start_date: Faker::Date.between(4.days.ago, 3.days.ago), end_date:Faker::Date.between(2.days.ago, Date.today), user: user )
 end
 puts "flat contracts created"
